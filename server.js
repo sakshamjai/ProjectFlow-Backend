c21d4a2a79a4cd0bdfcb31aada650e14640bf7d7
Saksham <sakshamjain3493@gmail.com>
2026-05-07 11:39:06 +0530

    port-error-fixed.

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ import dotenv from 'dotenv';
 import app from './src/app.js';
 import connectDB from './src/config/db.js';
 dotenv.config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const startServer = async () => {
     try{
         await connectDB();
-        app.listen(PORT, () => {
+        app.listen(PORT,"0.0.0.0", () => {
         console.log(`Server is running on port ${PORT}`);
         });
     }
